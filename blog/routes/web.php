c89697ed606ee1f9602b956d5963008dca231d98
tikanurannisa <tikaannisa23@gmail.com>
2019-03-22 10:00:25 +0700

    publish file index

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,33 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get ('/hello', function () {
+	return '<center><h1> HELLO WORLD <h1></center>';
+});
+
+Route::get('/welcome', function () {
+echo "Welcome";
+});
+
+Route::get('/show/{id}', function ($id) {
+echo "Nilai Parameter Adalah ".$id;
+});
+
+Route::get('/show/{id?}', function ($id=1) {
+echo "Nilai Parameter Adalah ".$id;
+});
+
+Route::get('/edit/{nama}', function ($nama) {
+echo "Nilai Parameter Adalah ".$nama;
+})->where('nama','[A-Za-z]+');
+
+Route::get('/index', function () {
+echo "<a href='".route('create')."'>Akses Route dengan nama </a>";
+});
+
+Route::get('/create', function () {
+echo "Route diakses menggunakan nama";
+})->name('create');
+
+Route::get('/produk', 'produkController@index');
